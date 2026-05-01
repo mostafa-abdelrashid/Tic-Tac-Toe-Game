74455906f9733b650e06fc7a0b0e5200a34c0bea
Yehia Sami <yehiasami352@gmail.com>
2026-05-01 03:02:54 +0300

    member 4 's tasks Done

--- main.cpp
+++ main.cpp
@@ -100,45 +100,113 @@ class AIPlayer: public Player{
     }
 };
 class Game{
+
     private:
+
     Board board;
     Player* currentPlayer;
     Player* player1;
     Player* player2;
     bool isComputerTurn;
+
+
     public:
+
     Game() : board(3){
         //To Do
     }
-    void start(){
-        //To Do
+
+    void start() {
+        showMenu();
+        while (!gameOver) {
+            board.display();
+            cout << currentPlayer->getName() << "'s turn (" << currentPlayer->getSymbol() << ")\n";
+            if (isComputerTurn)
+                handleAIMove((AIPlayer*)player2);
+            else
+                handleHumanMove(currentPlayer);
+            checkGameEnd();
+            if (!gameOver)
+                switchPlayer();
+        }
+        displayResult();
+        char again;
+        cout << "Play again? (y/n): ";
+        cin >> again;
+        if (again == 'y' || again == 'Y') {
+            reset();
+            start();
+        }
     }
+
     void showMenu(){
         //To Do
     }
+
     void setupPvP(){
         //To Do
     }
+
     void setupPvC(){
         //To Do
     }
-    void switchPlayer(){
-        //To Do
+
+    void switchPlayer() {
+        if (currentPlayer == player1) {
+            currentPlayer = player2;
+            isComputerTurn = true;
+        }
+        else {
+            currentPlayer = player1;
+            isComputerTurn = false;
+        }
     }
-    void handleHumanMove(Player* player){
-        //To Do
+
+    void handleHumanMove(Player* player) {
+        int row, col;
+        player->getMove(row, col);
+        while (!board.isValidMove(row, col)) {
+            cout << "Invalid move, try again.\n";
+            player->getMove(row, col);
+        }
+        board.makeMove(row, col, player->getSymbol());
     }
-    void handleAIMove(AIPlayer* aiPlayer){
-        //To Do
+
+    void handleAIMove(AIPlayer* aiPlayer) {
+        int row, col;
+        cout << aiPlayer->getName() << " is thinking...\n";
+        aiPlayer->getMove(row, col);
+        board.makeMove(row, col, aiPlayer->getSymbol());
+        cout << aiPlayer->getName() << " played at (" << row + 1 << ", " << col + 1 << ")\n";
     }
-    void checkGameEnd(){
-        //To Do
+
+    void checkGameEnd() {
+        if (board.checkWin(player1->getSymbol())) {
+            gameOver = true;
+        }
+        else if (board.checkWin(player2->getSymbol())) {
+            gameOver = true;
+        }
+        else if (board.isFull()) {
+            gameOver = true;
+        }
     }
-    void displayResult() const{
-        //To Do
+
+    void displayResult() const {
+        board.display();
+        if (board.checkWin(player1->getSymbol()))
+            cout << player1->getName() << " wins!\n";
+        else if (board.checkWin(player2->getSymbol()))
+            cout << player2->getName() << " wins!\n";
+        else
+            cout << "It's a draw!\n";
     }
-    void reset(){
-        //To Do
+
+    void reset() {
+        board.reset();
+        currentPlayer = player1;
+        isComputerTurn = false;
+        gameOver = false;
     }
 };
 int main(){
